8cff9b60050173c7f360ecf32572cbe314f8a03c
Vladyslav <vkadyrov@protonmail.com>
2023-03-16 18:35:15 -0400

    Client side hierarchy

--- todolist-client/src/components/App.js
+++ todolist-client/src/components/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './List';
 
 function App() {
   return (
---
+++ todolist-client/src/components/List.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Task from './Task';
+import Step from './Step';
+
+export default function List() {}
