bbac6a9be49d121cdedacd7ad5e10925363b20a4
Vladyslav <vkadyrov@protonmail.com>
2023-03-16 21:41:58 -0400

    Building server side

--- todolist-server/app/controllers/application_controller.rb
+++ todolist-server/app/controllers/application_controller.rb
@@ -1,36 +1,45 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
-  # Add routes
+  # Routes for Tasks
+
   get '/tasks' do
     tasks = Task.all.order(:created_at)
     tasks.to_json
   end
+
+  post '/tasks' do
+    task = Task.create(name: params[:name])
+    task.to_json
+  end
+
+  delete '/tasks/:id' do
+    task = Task.find(params[:id])
+    task.destroy
+    task.to_json
+  end
+
+  # Routes for Steps
 
   get '/steps' do
     steps = Step.all.order(:created_at)
     steps.to_json
   end
 
-  post '/tasks' do
-    message = Task.create(name: params[:name])
-    message.to_json
-  end
-
   post '/steps' do
-    message = Step.create(name: params[:name])
-    message.to_json
+    step = Step.create(name: params[:name])
+    step.to_json
   end
 
   patch '/steps/:id' do
-    message = Step.find(params[:id])
-    message.update(name: params[:name])
-    message.to_json
+    step = Step.find(params[:id])
+    step.update(name: params[:name])
+    step.to_json
   end
 
   delete '/steps/:id' do
-    message = Step.find(params[:id])
-    message.destroy
-    message.to_json
+    step = Step.find(params[:id])
+    step.destroy
+    step.to_json
   end
 end
